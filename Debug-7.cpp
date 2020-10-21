4383fc596b5f7d6080d84fa510d9c069e5c2ee66
Kaleb <72029196+kalebjaquez@users.noreply.github.com>
2020-10-21 11:25:04 -0500

    Update Debug-7.cpp
    I just added 2 double variables and then returned it I put // comments on it

--- Debug-7.cpp
+++ Debug-7.cpp
@@ -7,9 +7,13 @@ using namespace std;
 
 // This code is designed to approximate the square root of 2.
 // Remember that the square root of 2 is simply the number that square to 2.
-double square(int x){
-    cout << x*x;
-    return 0;
+double square(double x){
+	// the interger that is x should be a double for it can be a decimal.
+   double a =  x*x;
+	//cout doesnt need to be right at start of the code.
+	// I need to make a double to make x*x equal sommething.
+    return a;
+	// then you have to retunr a so that it will do x times x to get the square root of 2
 }
 double lowApprox = 1;
 double highApprox = 2;
